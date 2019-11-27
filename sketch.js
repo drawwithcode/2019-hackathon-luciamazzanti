7a444a8cecae186bb47b976a1f481202dcc5b1ce
Lucia Mazanti <luciamazzanti4@gmail.com>
2019-11-27 12:43:39 +0100

    change 2

--- sketch.js
+++ sketch.js
@@ -4,7 +4,6 @@ var myImage;
 var img;
 var img2;
 
-
 function preload() {
   mySong = loadSound("./assets/TG1_new.mp3");
   myImage = loadImage("./assets/tg1.png");
@@ -17,7 +16,6 @@ function setup() {
   angleMode(DEGREES);
 
   analyzer = new p5.Amplitude();
-
   analyzer.setInput(mySong);
 }
 
@@ -26,8 +24,9 @@ function draw() {
   background('#0F084B');
 
   volume = analyzer.getLevel();
-  volume = map(volume, 0, 1, 0, height); //we use height to make it as big as possible
+  volume = map(volume, 0, 1, 0, height);
 
+  //2 arcs behind the central ellipse
   stroke('#EE6C4D');
   noFill();
   strokeWeight(5);
@@ -35,6 +34,7 @@ function draw() {
   arc(windowWidth / 2, windowHeight / 2, volume + 450, volume + 300, 180, 360, OPEN);
   arc(windowWidth / 2, windowHeight / 2, volume + 500, volume + 350, 180, 360, OPEN);
 
+  //central ellipse
   fill('#26408B');
   noStroke();
   ellipse(windowWidth / 2, windowHeight / 2, volume + 400);
@@ -42,17 +42,19 @@ function draw() {
   imageMode(CENTER);
   image(myImage, windowWidth / 2, windowHeight / 2, myImage.width / 2 + volume, myImage.height / 2 + volume);
 
+  //2 arcs on top the central ellipse
   stroke('#EE6C4D');
   noFill();
   strokeWeight(5);
 
-
   arc(windowWidth / 2, windowHeight / 2, volume + 450, volume + 300, 0, 180, OPEN);
   arc(windowWidth / 2, windowHeight / 2, volume + 500, volume + 350, 0, 180, OPEN);
 
+  //2 musicians
   image(img, windowWidth / 6, windowHeight, img.width / 2, img.height / 2 * volume / 12);
   image(img2, windowWidth - windowWidth / 6, windowHeight, img2.width / 2, img2.height / 2 * volume / 12);
 
+  //song
   if (mouseIsPressed == true) {
     if (mySong.isPlaying() == false) {
       mySong.play();
